e268f2dedf37e13a19ced67197b0541fffac626a
Łukasz <luk.tucholski@gmail.com>
2020-01-02 05:29:09 +0100

    deploy settings for netlify

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,3 @@
-const path = require('path');
-
 module.exports = {
   transpileDependencies: ['vuetify'],
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      template: 'public/index.html',
-      filename: 'i-want-to-play-a-game.html',
-    },
-  },
 };
